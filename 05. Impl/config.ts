c3ea3a9a5adfe7da7ba8432410dd5b5866b0557a
jericholapac <jerichomaglimololapac@gmail.com>
2025-05-31 12:57:54 +0800

    Add files via upload

---
+++ 05. Impl/config.ts
@@ -0,0 +1,8 @@
+export const API_CONFIG = {
+  baseURL: 'http://192.168.110.174:3000/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
+}; 
